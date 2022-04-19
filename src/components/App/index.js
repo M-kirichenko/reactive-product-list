9901670af511b10682abc936c15fa97343ba6e9b
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-04-19 15:51:53 +0400

    added: 1.search, 2:back to initial list action

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,16 +7,32 @@ import "./app.css";
 class App extends Component {
   state = {
     products: [],
+    filtered: null,
   };
+
   async componentDidMount() {
     const { data: products } = await axios.get("./db/db.json");
     this.setState((state) => (state.products = products));
   }
+
+  search = (name) => {
+    const filtered = this.state.products.filter((product) => {
+      const productName = product.name.toLowerCase();
+      const inputName = name.toLowerCase();
+      return productName.includes(inputName);
+    });
+    this.setState((state) => (state.filtered = filtered));
+  };
+
   render() {
     return (
       <>
-        <Panel />
-        <ProductList products={this.state.products} />
+        <Panel onSearch={this.search} />
+        <ProductList
+          products={
+            this.state.filtered ? this.state.filtered : this.state.products
+          }
+        />
       </>
     );
   }
--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -2,12 +2,31 @@ import React, { Component } from "react";
 import "./panel.css";
 
 class Panel extends Component {
+  state = {
+    input: "",
+  };
+
   render() {
+    const { onSearch: search } = this.props;
     return (
       <div id="panel">
         <div id="panel-items">
-          <input type="text" className="panel-el" placeholder="Product name" />
-          <button type="button" className="panel-el">
+          <input
+            type="text"
+            className="panel-el"
+            placeholder="Product name"
+            onInput={({ target }) => {
+              if (!target.value.trim()) {
+                search("");
+                this.setState((state) => (state.input = ""));
+              } else this.setState((state) => (state.input = target.value));
+            }}
+          />
+          <button
+            type="button"
+            className="panel-el"
+            onClick={() => search(this.state.input)}
+          >
             Search
           </button>
           <strong>Total value of selected items: 0&nbsp;$</strong>
