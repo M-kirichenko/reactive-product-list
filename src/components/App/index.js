76215f2d4fba949ff7141689e8ab378f4234c400
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-04-20 15:30:03 +0400

    initial commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,19 +4,71 @@ import Panel from "../Panel";
 import ProductList from "../ProductList";
 import "./app.css";
 
+const productsAmount = localStorage.getItem("products")
+  ? JSON.parse(localStorage.getItem("products"))
+  : {};
+
 class App extends Component {
   state = {
     products: [],
+    filtered: null,
+    cart: {},
   };
+
   async componentDidMount() {
     const { data: products } = await axios.get("./db/db.json");
-    this.setState((state) => (state.products = products));
+    this.setState((state) => {
+      state.products = products;
+      state.cart = productsAmount;
+      return state;
+    });
   }
+
+  search = (name) => {
+    const filtered = this.state.products.filter((product) => {
+      const productName = product.name.toLowerCase();
+      const inputName = name.toLowerCase();
+      return productName.includes(inputName);
+    });
+    this.setState((state) => (state.filtered = filtered));
+  };
+
+  addProduct = (id) => {
+    if (productsAmount[id]) {
+      delete productsAmount[id];
+    } else {
+      productsAmount[id] = 1;
+    }
+    localStorage.setItem("products", JSON.stringify(productsAmount));
+    this.setState(
+      (state) => (state.cart = JSON.parse(localStorage.getItem("products")))
+    );
+  };
+
+  countSum = () => {
+    let sum = 0;
+    for (let key in this.state.cart) {
+      const prodInd = this.state.products.findIndex(
+        // eslint-disable-next-line eqeqeq
+        (product) => product.id == key
+      );
+      sum += this.state.products[prodInd].price * this.state.cart[key];
+    }
+    return sum;
+  };
+
   render() {
+    const totalPrice = this.countSum();
     return (
       <>
-        <Panel />
-        <ProductList products={this.state.products} />
+        <Panel onSearch={this.search} total={totalPrice} />
+        <ProductList
+          products={
+            this.state.filtered ? this.state.filtered : this.state.products
+          }
+          cart={this.state.cart}
+          onProductAdd={this.addProduct}
+        />
       </>
     );
   }
--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 import "./panel.css";
 
 class Panel extends Component {
+  state = {
+    input: "",
+  };
+
   render() {
+    const { onSearch, total } = this.props;
     return (
       <div id="panel">
         <div id="panel-items">
-          <input type="text" className="panel-el" placeholder="Product name" />
-          <button type="button" className="panel-el">
+          <input
+            type="text"
+            className="panel-el"
+            placeholder="Product name"
+            onInput={({ target }) =>
+              this.setState((state) => (state.input = target.value))
+            }
+          />
+          <button
+            type="button"
+            className="panel-el"
+            onClick={() => onSearch(this.state.input)}
+          >
             Search
           </button>
-          <strong>Total value of selected items: 0&nbsp;$</strong>
+          <strong>Total value of selected items: {total} &nbsp;$ </strong>
         </div>
       </div>
     );
